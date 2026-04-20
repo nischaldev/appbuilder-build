e02b5e9e829d3716ad416cb61d76bbb284358b9f
nischalbhandari12 <nischal.bhandari@mentorfriends.com>
2026-04-20 16:12:01 +0545

    fixed onesignal

--- OneSignalSDKWorker.js
+++ OneSignalSDKWorker.js
@@ -1,2 +1,7 @@
-importScripts("https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.sw.js");
+try {
+  importScripts("https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.sw.js");
+} catch (error) {
+  self.console?.warn?.("OneSignal worker SDK failed to load. Continuing without push support.", error);
+}
+
 importScripts("/sw.js");
